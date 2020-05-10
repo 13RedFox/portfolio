6cdc1e5f38ecbafe2fbdbfe308a3cb0fe1d99136
Serhii <sergei181989@gmail.com>
2020-05-10 15:34:03 +0300

    Create navigation & first-screen block :pen:

---
+++ 001_MoGo/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const navBar = document.getElementById("navBar");
+const burger = document.getElementById("burger");
+const mobileMenu = document.getElementById("mobileNav");
+
+document.addEventListener("scroll", () =>
+  window.pageYOffset < navBar.clientHeight
+    ? navBar.classList.remove("filled")
+    : navBar.classList.add("filled")
+);
+
+burger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("active");
+});
