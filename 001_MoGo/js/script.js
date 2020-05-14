5272970b64f1f3a8581d50dbe60a53b4e1be5bb5
Serhii <sergei181989@gmail.com>
2020-05-14 21:46:37 +0300

    Fixed mob__nav (beta version) :bug:

--- 001_MoGo/js/script.js
+++ 001_MoGo/js/script.js
@@ -1,9 +1,5 @@
 "use strict";
 
-const navBar = document.getElementById("navBar");
-const burger = document.getElementById("burger");
-const mobileMenu = document.getElementById("mobileNav");
-
 document.addEventListener("scroll", () => {
   if (window.pageYOffset < navBar.clientHeight) {
     navBar.classList.remove("filled");
@@ -14,6 +10,24 @@ document.addEventListener("scroll", () => {
   }
 });
 
-burger.addEventListener("click", () => {
-  mobileMenu.classList.toggle("active");
+jQuery(document).ready(function () {
+  if ($(".cd-stretchy-nav").length > 0) {
+    var stretchyNavs = $(".cd-stretchy-nav");
+
+    stretchyNavs.each(function () {
+      var stretchyNav = $(this),
+        stretchyNavTrigger = stretchyNav.find(".cd-nav-trigger");
+
+      stretchyNavTrigger.on("click", function (event) {
+        event.preventDefault();
+        stretchyNav.toggleClass("nav-is-visible");
+      });
+    });
+
+    $(document).on("click", function (event) {
+      !$(event.target).is(".cd-nav-trigger") &&
+        !$(event.target).is(".cd-nav-trigger span") &&
+        stretchyNavs.removeClass("nav-is-visible");
+    });
+  }
 });
